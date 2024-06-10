5e9c8af20b2ccbecb5eddda75f34c10ef8033540
kado1130 <kadoros1130@gmail.com>
2024-06-10 15:33:37 +0200

    massage app

--- App.js
+++ App.js
@@ -1,10 +1,115 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Location from "expo-location";
+import { StatusBar } from "expo-status-bar";
+import { Fontisto } from "@expo/vector-icons";
+import React, { useEffect } from "react";
+import {
+  ActivityIndicator,
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Dimensions,
+} from "react-native";
+
+const { width: SCREAN_WIDTH } = Dimensions.get("window");
+const API_KEY = "da7c9e1f412508fbdea92456b001a918";
+
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Rain: "rains",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
+};
 
 export default function App() {
+  const [city, setCity] = React.useState("Loading...");
+  const [days, setDays] = React.useState([]);
+  const [ok, setOk] = React.useState(true);
+
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({
+      accuracy: 5,
+    });
+    const location = await Location.reverseGeocodeAsync(
+      {
+        latitude,
+        longitude,
+      },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&exclude=alerts&units=metric`
+    );
+
+    const json = await response.json();
+    setDays(json.list);
+  };
+  useEffect(() => {
+    getWeather();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        {days.length === 0 ? (
+          <View
+            style={{
+              ...styles.day,
+              alignItems: "center",
+            }}
+          >
+            <ActivityIndicator
+              color="black"
+              style={{ marginTop: 10 }}
+              size="large"
+            />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <Text style={styles.dt}>{day.dt_txt}</Text>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "flex-end",
+                  justifyContent: "space-between",
+                  width: "100%",
+                }}
+              >
+                <Text style={styles.temp}>
+                  {parseFloat(day.main.temp).toFixed(1)}
+                </Text>
+                <Fontisto
+                  name={icons[day.weather[0].main]}
+                  size={130}
+                  color="black"
+                />
+              </View>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
+      </ScrollView>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +118,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "skyblue",
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 48,
+    fontWeight: "600",
+  },
+  day: {
+    width: SCREAN_WIDTH,
+    alignItems: "left",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 100,
+  },
+  description: {
+    marginTop: -20,
+    fontSize: 38,
+  },
+  tinyText: {
+    fontSize: 20,
+  },
+  dt: {
+    marginTop: 80,
+    marginBottom: -50,
+    fontSize: 15,
+  },
+  loading: {
+    width: SCREAN_WIDTH,
+    alignItems: "center",
   },
 });
